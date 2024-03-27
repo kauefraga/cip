2a3f5918f2f3db3fefd897b25dc72d0973085361
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-03-26 21:31:16 -0300

    script: remove unnecessary comments

--- release.sh
+++ release.sh
@@ -1,17 +1,15 @@
 #! /bin/bash
 
+# Please, execute this script at the root
+
 echo "Bundling and zipping..."
 
-# Verify if dist directory does not exist
 if [ ! -d "./cip" ]; then
   mkdir "cip"
 fi
 
-# Copy necessary files to the dist directory
 cp -r LICENSE index.html style.css manifest.json assets ./cip
 
-# Zip the dist directory
 zip -r cip.zip ./cip
 
-# Clean stuff
 rm -r ./cip
